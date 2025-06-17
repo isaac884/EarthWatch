88652ca977670f5d66081ae22d5ebda9eecde34f
Isaac Ngan <146541826+isaac884@users.noreply.github.com>
2025-06-18 00:46:45 +0800

    youtube.js

--- youtube.js
+++ youtube.js
@@ -2,6 +2,3 @@ document.querySelector('.menu-button').addEventListener('click', function () {
     document.querySelector('.sidebar').classList.toggle('active');
   });
 
-function scrollNext() {
-    document.getElementById('next').scrollIntoView({ behavior: 'smooth' });
-  }
