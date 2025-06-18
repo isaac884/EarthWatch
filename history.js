4f423ce7ae0620653ad3bc853f05414b9de1304d
Isaac Ngan <146541826+isaac884@users.noreply.github.com>
2025-06-18 14:59:52 +0800

    history.js

--- history.js
+++ history.js
@@ -50,6 +50,4 @@ document.querySelector('.menu-button').addEventListener('click', function () {
 document.querySelector('.sidebar').classList.toggle('active');
 });
 
-function scrollNext() {
-document.getElementById('next').scrollIntoView({ behavior: 'smooth' });
-}
+
