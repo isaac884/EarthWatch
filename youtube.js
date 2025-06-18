58106dc37a5492a2e8437af7da281cc9a170e392
Isaac Ngan <146541826+isaac884@users.noreply.github.com>
2025-06-18 16:34:05 +0800

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,3 +1,5 @@
+
+// This script toggles the sidebar visibility when the menu button is clicked.
 document.querySelector('.menu-button').addEventListener('click', function () {
     document.querySelector('.sidebar').classList.toggle('active');
   });
